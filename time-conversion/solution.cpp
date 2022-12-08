17d513036a413ded30e28e08dfb05c70f44f23b8
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-12-07 22:52:31 -0500

    Added Time Conversion

---
+++ time-conversion/solution.cpp
@@ -0,0 +1,36 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "../stdc++.h"
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+    cout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
